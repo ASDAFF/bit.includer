fed5e28418af0a15e0c38c3fb9ed2acfb87a31ad
verbovenko <verbovenko@it-sfera.ru>
2015-01-23 21:05:59 +0300

    add descriptions

--- itsfera.includer/include.php
+++ itsfera.includer/include.php
@@ -1,23 +1,2 @@
 <?php
-
-/*
-class ItsferaIncluderHandlers {
-
-    //AddEventHandler("fileman", "OnIncludeHTMLEditorScript", "OnIncludeHTMLEditorScriptHandler");
-    function OnIncludeHTMLEditorScriptHandler()
-    {
-        $script_filename = '/slider.js';
-        echo '<script type="text/javascript" src="'.$script_filename.'?v='.@filemtime($_SERVER['DOCUMENT_ROOT'].$script_filename).'"></script>';
-    }
-
-    //AddEventHandler("main", "OnEndBufferContent", "OnEndBufferContentHandler");
-    public static function OnEndBufferContentHandler(&$content)
-    {
-        if (!defined("ADMIN_SECTION")){
-
-            $content = 'work!'.$content;
-            //sliderIncluder($content,CCustomProject::getIBlockIdByCode('ru_gallery'),"[slider]","[/slider]");
-            //photoIncluder($content,CCustomProject::getIBlockIdByCode('ru_gallery'),"[photo]","[/photo]");
-        }
-    }
-}*/
+?>
--- itsfera.includer/lang/ru/install/index.php
+++ itsfera.includer/lang/ru/install/index.php
@@ -2,7 +2,7 @@
 $MESS["CONTENT_INCLUDER_MODULE_NAME"] = "Itsfera.Includer";
 $MESS["CONTENT_INCLUDER_MODULE_DESC"] = "";
 $MESS["CONTENT_INCLUDER_PARTNER_URI"] = "http://web.it-sfera.ru";
-$MESS["CONTENT_INCLUDER_PARTNER_NAME"] = "IT-Sfera";
+$MESS["CONTENT_INCLUDER_PARTNER_NAME"] = "It-Sfera";
 $MESS['CONTENT_INCLUDER_MODULE_INSTALL_TITLE'] = "Установка модуля ";
 $MESS['CONTENT_INCLUDER_MODULE_UNINSTALL_TITLE'] = "Деинсталяция модуля ";
 $MESS['COPY_ERROR_DIR_IS_NOR_WRITABLE'] = "Недосточно прав для копирования шаблонов в директорию: #PATH#. Для работы модуля, вам нужно переписать шаблоны стандартных компонентов из папки модуля: #FROM#.";
