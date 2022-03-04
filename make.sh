e19bf5d57202b3b1c15b6afd4d5788ae8179256b
ASDAFF <asdaff.asad@yandex.ru>
2022-03-04 16:27:28 +0500

    up

--- make.sh
+++
@@ -1,4 +0,0 @@
-mkdir .last_version
-cp -r bit.includer/. .last_version
-find .last_version -name '*.php' -exec iconv --verbose -f utf-8 -t windows-1251 -o {} {} \;
-zip -r .last_version.zip .last_version
